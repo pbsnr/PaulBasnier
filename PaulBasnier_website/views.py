45b2962045f8250b8a7f3fe975b31740f90263da
pbsnr <pbsnr.dev@gmail.com>
2019-09-23 23:44:36 +0200

    creeating two pages, started to play with djano

--- PaulBasnier_website/views.py
+++ PaulBasnier_website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', {'date': datetime.datetime.now()})
+
+
+def current_datetime(request, pseudo):
+    now = datetime.datetime.now()
+    return render(request, 'datetime.html', locals())
+
